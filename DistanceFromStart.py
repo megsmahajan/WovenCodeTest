899bd585c2781caeaa38815bd1c212feb5022362
meghana <mahajan.meghanam@gmail.com>
2020-02-16 21:58:11 +1100

    Adding: Special cases, fault tolerance, exception handling.

--- DistanceFromStart.py
+++ DistanceFromStart.py
@@ -4,57 +4,63 @@ from ShortestPath import *
 import numpy as np
 
 def find_smallest_dist(final_directions):
-    # Defining directions for the robot.
-    N = 0
-    E = 1
-    S = 2
-    W = 3
 
-    # Initially, the robot will be facing North.
-    dir = N
+    if final_directions != "":
+        # Defining directions for the robot.
+        N = 0
+        E = 1
+        S = 2
+        W = 3
 
-    # Defining starting position as 0,0.
-    x = 0
-    y = 0
+        # Initially, the robot will be facing North.
+        dir = N
 
-    # Traverse the path given for robot
-    for move in final_directions:
+        # Defining starting position as 0,0.
+        x = 0
+        y = 0
 
-        # Find current move
+        # Traverse the path given for robot
+        for move in final_directions:
 
-        # If move is left or right, then change direction
-        if move == 'R':
-            dir = (dir + 1) % 4
-        elif move == 'L':
-            dir = (4 + dir - 1) % 4
+            # Find current move
 
-        # If move is forward or backward, then change x or y according to the current direction
-        # Final stopping position of the robot is in a 2D Matrix and not in the co-ordinate system.
-        else:
-            if dir == N and move == "F":
-                x -= 1
-            elif dir == N and move == "B":
-                x += 1
-            elif dir == E and move == "F":
-                y += 1
-            elif dir == E and move == "B":
-                y -= 1
-            elif dir == S and move == "F":
-                x += 1
-            elif dir == S and move == "B":
-                x -= 1
-            elif dir == W and move == "F":
-                y -= 1
-            elif dir == W and move == "B":
-                y += 1
+            # If move is left or right, then change direction
+            if move == 'R':
+                dir = (dir + 1) % 4
+            elif move == 'L':
+                dir = (4 + dir - 1) % 4
 
-    print("Final position: ", (x, y))
+            # If move is forward or backward, then change x or y according to the current direction
+            # Final stopping position of the robot is in a 2D Matrix and not in the co-ordinate system.
+            else:
+                if dir == N and move == "F":
+                    x -= 1
+                elif dir == N and move == "B":
+                    x += 1
+                elif dir == E and move == "F":
+                    y += 1
+                elif dir == E and move == "B":
+                    y -= 1
+                elif dir == S and move == "F":
+                    x += 1
+                elif dir == S and move == "B":
+                    x -= 1
+                elif dir == W and move == "F":
+                    y -= 1
+                elif dir == W and move == "B":
+                    y += 1
 
-    # The final stopping position of the robot can be in negative space as the robot is moving freely in a 2D space.
-    # But, the distance from the origin of any particular point in negative space is same as the absolute distance.
+        print("Final position: ", (x, y))
 
-    # Passing absolute values of x,y co-ordinates to find the shortest path from the origin.
-    ShortestPath(abs(x), abs(y)).bfs(np.zeros((abs(x) + 1, abs(y) + 1)), 0, 0, abs(x), abs(y))
+        # The final stopping position of the robot can be in negative space as the robot is moving freely in a 2D space.
+        # But, the distance from the origin of any particular point in negative space is same as the absolute distance.
+
+        # Passing absolute values of x,y co-ordinates to find the shortest path from the origin.
+        ShortestPath(abs(x), abs(y)).bfs(np.zeros((abs(x) + 1, abs(y) + 1)), 0, 0, abs(x), abs(y))
+
+    else:
+        print("Couldn't find shortest path because of an invalid move")
+
 
 
 
@@ -72,6 +78,11 @@ def main():
 
         for d in each_position:
             m = re.split('(\d+)', d)
+
+            if (m[0] == "L" or m[0] == "R") and int(m[1]) >1:
+                print("Invalid move, you cannot turn left or right more than once in one move.")
+                final_directions = ""
+                break
             final_directions = final_directions + (m[0] * int(m[1]))
 
         find_smallest_dist(final_directions)
